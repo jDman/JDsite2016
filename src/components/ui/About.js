053f22cc8cff3788e4723aa1d48445169aff7f70
Jay Davis <davis.jaym@gmail.com>
2016-05-09 10:39:54 +0100

    Updated about page

--- src/components/ui/About.js
+++ src/components/ui/About.js
@@ -9,7 +9,9 @@ class About extends Component{
       <div className="mdl-grid portfolio-max-width">
         <div className="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
           <div className="mdl-card__title">
-              <h2 className="mdl-card__title-text">About</h2>
+              <h2 className="mdl-card__title-text">
+                Please allow me to introduce myself...
+              </h2>
           </div>
 
           <div className="about-title-image mdl-card__media">
@@ -20,21 +22,27 @@ class About extends Component{
 
           <div className="mdl-grid portfolio-copy">
               <h3 className="mdl-cell mdl-cell--12-col mdl-typography--headline">
-                An introduction
+                I am a man...
               </h3>
 
               <div className="mdl-cell mdl-cell--8-col mdl-card__supporting-text
                 no-padding">
                 <p>
-                  My goal is to write elegant, semantic code that creates
-                  beautiful web sites and applications. These are wonderfully
-                  interactive and built with mobile first in mind.
+                  ... who loves modular, maintainable code.
                 </p>
 
                 <p>
-                  With 5 years commercial experience in the web industry,
-                  I have the knowledge to make modern websites that stand out,
-                  whilst being compatable across multiple browsers and devices.
+                  Blending the latest technologies to mold modern, snappy
+                  user interfaces is the nature of my game.
+                </p>
+
+                <p>
+                  I have been on the front-end scene for over 5 exciting years.
+                  Seeing the landscape of web software development change.
+                </p>
+
+                <p>
+                  Pleased to meet you!
                 </p>
               </div>
 
@@ -54,23 +62,72 @@ class About extends Component{
               </div>
 
               <h3 className="mdl-cell mdl-cell--12-col mdl-typography--headline">
-                Irure mollit est sit labore
+                So what can I do for you exactly?
               </h3>
 
               <div className="mdl-cell mdl-cell--8-col mdl-card__supporting-text
-                no-padding ">
+                no-padding">
                   <p>
-                      Excepteur reprehenderit sint exercitation ipsum consequat
-                      qui sit id velit elit. Velit anim eiusmod labore sit amet.
-                      Voluptate voluptate irure occaecat deserunt incididunt
-                      esse in. Sunt velit aliquip sunt elit ex nulla
-                      reprehenderit qui ut eiusmod ipsum do. Duis veniam
-                      reprehenderit laborum occaecat id proident nulla veniam.
-                      Duis enim deserunt voluptate aute veniam sint pariatur
-                      exercitation. Irure mollit est sit labore est deserunt
-                      pariatur duis aute laboris cupidatat. Consectetur consequat
-                      esse est sit veniam adipisicing ipsum enim irure.
+                    If you're looking to solve problems for your user interface
+                    using the latest development tools, whether it's using a
+                    javascript framework like AngularJS or build tools like
+                    Gulp or Webpack, get in touch!
                   </p>
+
+                  <h4>Skills</h4>
+                    <ul className="about-skills">
+                      <li>
+                        <span className="devicons devicons-javascript"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-angular"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-backbone"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-bootstrap"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-sass"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-css3"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-html5"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-grunt"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-gulp"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-npm"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-bower"></span>
+                      </li>
+
+                      <li>
+                        <span className="devicons devicons-jira"></span>
+                      </li>
+
+                      <li>
+                        ... and more!
+                      </li>
+                    </ul>
               </div>
           </div>
         </div>
